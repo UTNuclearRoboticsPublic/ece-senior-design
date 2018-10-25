f6a7e13acef7ecdcd0792b2d3e84214560b05e10
John Sigmon <johnsigmon@gmail.com>
2018-10-24 19:26:17 -0500

    rename ros install script

--- ros_install.sh
+++ ros_install.sh
@@ -2,7 +2,9 @@
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-key adv \
+  --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+  --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 sudo apt-get update
 
@@ -15,5 +17,3 @@ echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
-
-
