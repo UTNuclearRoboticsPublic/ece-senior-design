4fbecae2962c16c9c37ef649633ca7e8727002f9
jsigee87 <johnsigmon@gmail.com>
2018-11-18 16:57:35 -0600

    usb cam

--- clean-install.sh
+++ clean-install.sh
@@ -33,7 +33,7 @@ else
 fi
 
 #scriptdir="$(dirname "$0")"
-#cd "$scriptdir" 
+#cd "$scriptdir"
 #I think this is unnecessary^^
 
 # Take the catkin workspace as a parameter from the user
@@ -60,83 +60,83 @@ CONFIG="config"
 #####################################################################
 dpkg -s git &> /dev/null
 if [ $? -eq 0 ]; then
-    echo "[INFO: $MYFILENAME $LINENO ] git is already installed, skipping installation." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] git is already installed, skipping installation." >> $LOGFILE
 else
-    echo "[INFO: $MYFILENAME $LINENO ] Installing git." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installing git." >> $LOGFILE
     sudo apt-get install git &&
-    echo "[INFO: $MYFILENAME $LINENO ] Installed git." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installed git." >> $LOGFILE
 fi
 
 dpkg -s python-catkin-pkg &> /dev/null
 if [ $? -eq 0 ]; then
-    echo "[INFO: $MYFILENAME $LINENO ] python-catkin-pkg is already installed, skipping installation." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] python-catkin-pkg is already installed, skipping installation." >> $LOGFILE
 else
-    echo "[INFO: $MYFILENAME $LINENO ] Installing python-catkin-pkg." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installing python-catkin-pkg." >> $LOGFILE
 	sudo apt-get install python-catkin-pkg &&
-    echo "[INFO: $MYFILENAME $LINENO ] Installed python-catkin-pkg." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installed python-catkin-pkg." >> $LOGFILE
 fi
 
 dpkg -s cmake &> /dev/null
 if [ $? -eq 0 ]; then
-    echo "[INFO: $MYFILENAME $LINENO ] cmake is already installed, skipping installation." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] cmake is already installed, skipping installation." >> $LOGFILE
 else
-    echo "[INFO: $MYFILENAME $LINENO ] Installing cmake." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installing cmake." >> $LOGFILE
 	sudo apt-get install cmake &&
-    echo "[INFO: $MYFILENAME $LINENO ] Installed cmake." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installed cmake." >> $LOGFILE
 fi
 
 dpkg -s python-empy &> /dev/null
 if [ $? -eq 0 ]; then
-    echo "[INFO: $MYFILENAME $LINENO ] python-empy is already installed, skipping installation." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] python-empy is already installed, skipping installation." >> $LOGFILE
 else
-    echo "[INFO: $MYFILENAME $LINENO ] Installing python-empy." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installing python-empy." >> $LOGFILE
 	sudo apt-get install python-empy &&
-    echo "[INFO: $MYFILENAME $LINENO ] Installed python-empy." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installed python-empy." >> $LOGFILE
 fi
 
 dpkg -s v4l-utils &> /dev/null
 if [ $? -eq 0 ]; then
-    echo "[INFO: $MYFILENAME $LINENO ] v4l-utils is already installed, skipping installation." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] v4l-utils is already installed, skipping installation." >> $LOGFILE
 else
-    echo "[INFO: $MYFILENAME $LINENO ] Installing v4l-utils." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installing v4l-utils." >> $LOGFILE
 	sudo apt-get install v4l-utils &&
-    echo "[INFO: $MYFILENAME $LINENO ] Installed v4l-utils." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installed v4l-utils." >> $LOGFILE
 fi
 
 dpkg -s python-nose &> /dev/null
 if [ $? -eq 0 ]; then
-    echo "[INFO: $MYFILENAME $LINENO ] python-nose is already installed, skipping installation." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] python-nose is already installed, skipping installation." >> $LOGFILE
 else
-    echo "[INFO: $MYFILENAME $LINENO ] Installing python-nose." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installing python-nose." >> $LOGFILE
 	sudo apt-get install python-nose &&
-    echo "[INFO: $MYFILENAME $LINENO ] Installed python-nose." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installed python-nose." >> $LOGFILE
 fi
 
 dpkg -s python-setuptools &> /dev/null
 if [ $? -eq 0 ]; then
-    echo "[INFO: $MYFILENAME $LINENO ] python-setuptools is already installed, skipping installation." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] python-setuptools is already installed, skipping installation." >> $LOGFILE
 else
-    echo "[INFO: $MYFILENAME $LINENO ] Installing python-setuptools." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installing python-setuptools." >> $LOGFILE
 	sudo apt-get install python-setuptools &&
-    echo "[INFO: $MYFILENAME $LINENO ] Installed python-setuptools." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installed python-setuptools." >> $LOGFILE
 fi
 
 dpkg -s libgtest-dev &> /dev/null
 if [ $? -eq 0 ]; then
-    echo "[INFO: $MYFILENAME $LINENO ] libgtest-dev is already installed, skipping installation." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] libgtest-dev is already installed, skipping installation." >> $LOGFILE
 else
-    echo "[INFO: $MYFILENAME $LINENO ] Installing libgtest-dev." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installing libgtest-dev." >> $LOGFILE
 	sudo apt-get install libgtest-dev &&
-    echo "[INFO: $MYFILENAME $LINENO ] Installed libgtest-dev." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installed libgtest-dev." >> $LOGFILE
 fi
 
 dpkg -s build-essential &> /dev/null
 if [ $? -eq 0 ]; then
-    echo "[INFO: $MYFILENAME $LINENO ] build-essential is already installed, skipping installation." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] build-essential is already installed, skipping installation." >> $LOGFILE
 else
-    echo "[INFO: $MYFILENAME $LINENO ] Installing build-essential." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installing build-essential." >> $LOGFILE
 	sudo apt-get install build-essential &&
-    echo "[INFO: $MYFILENAME $LINENO ] Installed build-essential." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installed build-essential." >> $LOGFILE
 fi
 
 #####################################################################
@@ -146,11 +146,11 @@ bash -i $INSTALL/ros-install.sh ../$CATKIN_RELATIVE
 
 dpkg -s ros-kinetic-catkin &> /dev/null
 if [ $? -eq 0 ]; then
-    echo "[INFO: $MYFILENAME $LINENO ] ros-kinetic-catkin is already installed, skipping installation." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] ros-kinetic-catkin is already installed, skipping installation." >> $LOGFILE
 else
-    echo "[INFO: $MYFILENAME $LINENO ] Installing ros-kinetic-catkin." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installing ros-kinetic-catkin." >> $LOGFILE
     sudo apt-get install ros-kinetic-catkin
-    echo "[INFO: $MYFILENAME $LINENO ] Installed ros-kinetic-catkin." >> $LOGFILE
+    echo "[INFO: $MYFILENAME $LINENO] Installed ros-kinetic-catkin." >> $LOGFILE
 fi
 
 #####################################################################
@@ -161,4 +161,4 @@ bash -i $INSTALL/textured-sphere-install.sh -c $CATKIN_RELATIVE -l $LOGFILE
 bash -i $INSTALL/vive-plugin-install.sh -c $CATKIN_RELATIVE -l $LOGFILE
 
 source /opt/ros/kinetic/setup.bash
-echo "[INFO: $MYFILENAME $LINENO ] Install finished." >> $LOGFILE
+echo "[INFO: $MYFILENAME $LINENO] Install finished." >> $LOGFILE
--- install/usb-cam-install.sh
+++ install/usb-cam-install.sh
@@ -1,27 +1,53 @@
 #!/usr/bin/env bash
-
+#
 # Authors: John Sigmon and Daniel Diamont
-# Last modified 11/5/18
-
+# Last modified 11/18/18
+#
 # Purpose:
 #	This script installs the ROS package usb_cam if it is not already installed
 
-
-# Check that user passed in catkin workspace path
-if [ $# -eq 1 ]; 
+#####################################################################
+# Parse args
+#####################################################################
+if [ $# -lt 2 ];
 then
-	CATKIN_RELATIVE=${1%/}
-else
 	echo "Usage: usb-cam-install.sh <path to catkin workspace>"
 	exit 1
 fi
 
-FILENAME="usb-cam-install.sh"
-MYFULLPATH=$(locate $FILENAME)	# Finds all copies of this file!!!!!!!
-MYPATH=${MYFULLPATH%/*}
-#ROOT=$PWD
-#CATKIN=$ROOT/$CATKIN_RELATIVE	
-CATKIN=$CATKIN_RELATIVE	
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -c|--catkin)
+    CATKIN=${2%/} # strip trailing slash
+    shift # past argument
+    shift # past value
+    ;;
+    -l|--logfile)
+    LOGFILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+esac
+done
+
+#####################################################################
+# Configure log and vars
+#####################################################################
+timestamp() {
+    date +"%T"
+}
+MYFILENAME="usb-install.sh"
+if [[ -z "$LOGFILE" ]];
+then
+    LOGFILE="log$(timestamp)"$MYFILENAME".txt"
+fi
+
+MYFULLPATH=$(readlink -f $MYFILENAME)	# More portable
+#MYFULLPATH=$(locate $FILENAME)	# Finds all copies of this file!!!!!!!
+MYPATH=${MYFULLPATH%/*} # strip filename
 BUILD="build"
 SRC="src"
 DEST="usb_cam"
@@ -30,28 +56,51 @@ CONFIG="config"
 SINGLECAM="single-cam.launch"
 DUALCAM="dual-cam.launch"
 
-# Create catkin workspace subdirectories
-#cd "$CATKIN"
-mkdir -p "$CATKIN"/"$BUILD"
-mkdir -p "$CATKIN"/"$SRC"
-
-# Setup usb_cam
-#cd $SRC
-if [ ! -d "$CATKIN"/"$SRC"/"$DEST" ];
+#####################################################################
+# Make Catkin dirs if not there and clone usb-cam
+#####################################################################
+if [ ! -d "$CATKIN"/"$BUILD" ];
 then
-    git clone https://github.com/ros-drivers/usb_cam.git "$CATKIN"/"$SRC"/"$DEST"/
+	echo "[INFO: $MYFILENAME $LINENO] Making "$BUILD" dir in catkin workspace at "$CATKIN"" >> $LOGFILE
+    mkdir -p "$CATKIN"/"$BUILD"
 fi
 
-#cd $MYPATH
+if [ ! -d "$CATKIN"/"$SRC" ];
+then
+	echo "[INFO: $MYFILENAME $LINENO] Making "$SRC" dir in catkin workspace at "$CATKIN"" >> $LOGFILE
+    mkdir -p "$CATKIN"/"$SRC"
+fi
 
-# Take dual-cam.launch file located in config/
-# 	and place it in usb_cam 'launch' directory
-cp $MYPATH/$CONFIG/$SINGLECAM $CATKIN/$SRC/$DEST/$LAUNCH/ #$SINGLECAM
-cp $MYPATH/$CONFIG/$DUALCAM $CATKIN/$SRC/$DEST/$LAUNCH/ #$DUALCAM
-   
-#if [ ! -f "$CATKIN/$SRC/$DEST/$LAUNCH/$DUALCAM" ];
-#then
-#    echo -e $DUALCAM_FILE >> $CATKIN/$SRC/$DEST/$LAUNCH/$DUALCAM
-#fi
+if [ ! -d "$CATKIN"/"$SRC"/"$DEST" ];
+then
+	echo "[INFO: $MYFILENAME $LINENO] Installing usb-cam into "$CATKIN"/"$SRC"/"$DEST"" >> $LOGFILE
+    git clone https://github.com/ros-drivers/usb_cam.git "$CATKIN"/"$SRC"/"$DEST"/ &&
+	echo "[INFO: $MYFILENAME $LINENO] Installed usb-cam into "$CATKIN"/"$SRC"/"$DEST"" >> $LOGFILE
+fi
 
-echo "USB cam installed."
+#####################################################################
+# Copy launch files to usb_cam 'launch' dir
+#####################################################################
+if [ ! -f "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/"$SINGLECAM" ];
+then
+	echo "[INFO: $MYFILENAME $LINENO] Copying "$SINGLECAM" to "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/"$SINGLECAM"" >> $LOGFILE
+    cp $MYPATH/$CONFIG/$SINGLECAM $CATKIN/$SRC/$DEST/$LAUNCH/
+	if [[ $? != 0 ]]; 
+    then 
+        echo "[INFO: $MYFILENAME $LINENO] Copy "$SINGLECAM" to "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/"$SINGLECAM" failed." >> $LOGFILE
+    fi
+else
+	echo "[INFO: $MYFILENAME $LINENO] "$SINGLECAM" already in "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/ , not copying." >> $LOGFILE
+fi
+
+if [ ! -f "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/"$DUALCAM" ];
+then
+	echo "[INFO: $MYFILENAME $LINENO] Copying "$DUALCAM" to "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/"$DUALCAM"" >> $LOGFILE
+    cp $MYPATH/$CONFIG/$DUALCAM $CATKIN/$SRC/$DEST/$LAUNCH/
+	if [[ $? != 0 ]]; 
+    then 
+        echo "[INFO: $MYFILENAME $LINENO] Copy "$DUALCAM" to "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/"$DUALCAM" failed." >> $LOGFILE
+    fi
+else
+	echo "[INFO: $MYFILENAME $LINENO] "$DUALCAM" already in "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/ , not copying." >> $LOGFILE
+fi
