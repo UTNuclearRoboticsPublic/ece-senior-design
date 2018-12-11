82bb0bda1687c74c0430ee370939652a438cd449
btandersen383 <btandersen@utexas.edu>
2018-12-11 15:46:18 -0600

    fix script errors, quotes, etc

--- vive/install/utils/cv_video_stream_install.sh
+++ vive/install/utils/cv_video_stream_install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #
 # Author: Kate Baumli 
-# Last modified 11/20/18
+# Modified: Beathan Andersen
+# Last modified 12/11/18
 #
 # Purpose:
 #	This script installs the ROS package video_stream_opencv if it is not already installed
@@ -42,13 +43,10 @@ timestamp() {
 MYFILENAME="cv-video-stream-install.sh"
 if [[ -z "$LOGFILE" ]];
 then
-    LOGFILE="log$(timestamp)"$MYFILENAME".txt"
+    LOGFILE="log$(timestamp)$MYFILENAME.txt"
 fi
 
-#MYFULLPATH=$(readlink -f $MYFILENAME)	# More portable
-#MYFULLPATH=$(locate $MYFILENAME)	# Finds all copies of this file!!!!!!!
 MYPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
 BUILD="build"
 SRC="src"
 DEST="video_stream_opencv"
@@ -62,46 +60,44 @@ DUALCAM="dual-cam.launch"
 #####################################################################
 if [ ! -d "$CATKIN"/"$BUILD" ];
 then
-	echo "[INFO: $MYFILENAME $LINENO] Making "$BUILD" dir in catkin workspace at "$CATKIN"" >> $LOGFILE
+	echo "[INFO: $MYFILENAME $LINENO] Making $BUILD dir in catkin workspace at $CATKIN" >> "$LOGFILE"
     mkdir -p "$CATKIN"/"$BUILD"
 fi
 
 if [ ! -d "$CATKIN"/"$SRC" ];
 then
-	echo "[INFO: $MYFILENAME $LINENO] Making "$SRC" dir in catkin workspace at "$CATKIN"" >> $LOGFILE
+	echo "[INFO: $MYFILENAME $LINENO] Making $SRC dir in catkin workspace at $CATKIN" >> "$LOGFILE"
     mkdir -p "$CATKIN"/"$SRC"
 fi
 
 if [ ! -d "$CATKIN"/"$SRC"/"$DEST" ];
 then
-	echo "[INFO: $MYFILENAME $LINENO] Installing video_stream_opencv into "$CATKIN"/"$SRC"/"$DEST"" >> $LOGFILE
+	echo "[INFO: $MYFILENAME $LINENO] Installing video_stream_opencv into $CATKIN/$SRC/$DEST" >> "$LOGFILE"
     git clone https://github.com/ros-drivers/video_stream_opencv.git "$CATKIN"/"$SRC"/"$DEST"/ &&
-	echo "[INFO: $MYFILENAME $LINENO] Installed video_stream_opencv into "$CATKIN"/"$SRC"/"$DEST"" >> $LOGFILE
+	echo "[INFO: $MYFILENAME $LINENO] Installed video_stream_opencv into $CATKIN/$SRC/$DEST" >> "$LOGFILE"
 fi
 
 #####################################################################
 # Copy launch files to video_stream_opencv 'launch' dir
 #####################################################################
-if [ ! -f "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/"$SINGLECAM" ];
+if [ ! -f "$CATKIN"/$SRC/"$DEST"/"$LAUNCH"/"$SINGLECAM" ];
 then
-	echo "[INFO: $MYFILENAME $LINENO] Copying "$SINGLECAM" to "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/"$SINGLECAM"" >> $LOGFILE
-    cp $MYPATH/$CONFIG/$SINGLECAM $CATKIN/$SRC/$DEST/$LAUNCH/
-	if [[ $? != 0 ]];
+    echo "[INFO: $MYFILENAME $LINENO] Copying $SINGLECAM to $CATKIN/$SRC/$DEST/$LAUNCH/$SINGLECAM" >> "$LOGFILE"
+    if ! cp "$MYPATH"/"$CONFIG"/"$SINGLECAM" "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/
     then
-        echo "[ERROR: $MYFILENAME $LINENO] Copy "$SINGLECAM" to "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/"$SINGLECAM" failed." >> $LOGFILE
+        echo "[ERROR: $MYFILENAME $LINENO] Copy $SINGLECAM to $CATKIN/$SRC/$DEST/$LAUNCH/$SINGLECAM failed." >> "$LOGFILE"
     fi
 else
-	echo "[INFO: $MYFILENAME $LINENO] "$SINGLECAM" already in "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/ , not copying." >> $LOGFILE
+	echo "[INFO: $MYFILENAME $LINENO] $SINGLECAM already in $CATKIN/$SRC/$DEST/$LAUNCH/ , not copying." >> "$LOGFILE"
 fi
 
-if [ ! -f "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/"$DUALCAM" ];
+if [ ! -f "$CATKIN"/$SRC/"$DEST"/"$LAUNCH"/"$DUALCAM" ];
 then
-	echo "[INFO: $MYFILENAME $LINENO] Copying "$DUALCAM" to "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/"$DUALCAM"" >> $LOGFILE
-    cp $MYPATH/$CONFIG/$DUALCAM $CATKIN/$SRC/$DEST/$LAUNCH/
-	if [[ $? != 0 ]];
+    echo "[INFO: $MYFILENAME $LINENO] Copying $DUALCAM to $CATKIN/$SRC/$DEST/$LAUNCH/$DUALCAM" >> "$LOGFILE"
+    if ! cp "$MYPATH"/"$CONFIG"/"$DUALCAM" "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/
     then
-        echo "[ERROR: $MYFILENAME $LINENO] Copy "$DUALCAM" to "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/"$DUALCAM" failed." >> $LOGFILE
+        echo "[ERROR: $MYFILENAME $LINENO] Copy $DUALCAM to $CATKIN/$SRC/$DEST/$LAUNCH/$DUALCAM failed." >> "$LOGFILE"
     fi
 else
-	echo "[INFO: $MYFILENAME $LINENO] "$DUALCAM" already in "$CATKIN"/"$SRC"/"$DEST"/"$LAUNCH"/ , not copying." >> $LOGFILE
+	echo "[INFO: $MYFILENAME $LINENO] $DUALCAM already in $CATKIN/$SRC/$DEST/$LAUNCH/ , not copying." >> "$LOGFILE"
 fi
