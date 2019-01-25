4bdd22108868b599bbc2222897e54a0d98909d71
btandersen383 <btandersen@utexas.edu>
2019-01-25 13:20:42 -0600

    replaced lsb_release -sc with xenial

--- vive/install/utils/ros_install.sh
+++ vive/install/utils/ros_install.sh
@@ -51,8 +51,9 @@ sudo apt-get update
 
 if ! dpkg -s ros-kinetic-desktop-full > /dev/null
 then
+    # Replaced $(lsb_release -sc) with xenial 
     # shellcheck disable=SC2016
-    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list'
     sudo apt-key adv \
         --keyserver hkp://ha.pool.sks-keyservers.net:80 \
         --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
