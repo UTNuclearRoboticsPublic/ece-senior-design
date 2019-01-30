d7eef1a20e0a34e0148e1b5b6aa5a5e494077790
John Sigmon <johnsigmon@gmail.com>
2019-01-30 17:38:04 -0600

    Performed network configuration file swapping in closed loop.

--- vive/base_launch.sh
+++ vive/base_launch.sh
@@ -1,28 +1,26 @@
 #!/usr/bin/env bash
 #####################################################################
-# Purpose: Launch base station part of the system including rviz, 
-#          textured sphere, network, and VR plugin
-# Authors: Kate Baumli, Daniel Diamont, Caleb Johnson
-# Date:    01/28/2019
+# Purpose:       Launch base station part of the system including 
+#                rviz, textured sphere, network, and VR plugin
+# Authors:       Kate Baumli, Daniel Diamont, Caleb Johnson
+# Last modified: 01/20/2019 by Kate
 #####################################################################
 
 #####################################################################
 # Parse args
 #####################################################################
-if [ $# -ne 6 ];
+if [ $# -lt 1 ];
 then
-	echo "Usage: base_launch.sh <-c|--catkin path to catkin workspace> [-l|--logfile logfile]"
-	echo "OR  base_launch.sh <-c|--catkin path to catkin workspace> [-l|--logfile logfile] [-b basehostname] [-bip baseip] [-r robohostname] [-rip roboip]"
+	echo "Usage: base_launch.sh <-c|--catkin path to catkin workspace> [-l|--logfile logfile] [-b basehostname] [-bip baseip] [-r robohostname] [-rip roboip]"
     exit 1
 fi
 
 while [[ $# -gt 0 ]]
 do
 key="$1"
-
 case $key in
     -c|--catkin)
-    CATKIN=${2%/} # strip trailing slash
+    CATKIN=${2%/} # strip trailing slash 
     shift # past argument
     shift # past value
     ;;
@@ -54,25 +52,44 @@ case $key in
 
 esac
 done
-
 # TODO Error checking make sure catkin and logfile exist, and flagging to configure network if BASE/ROBO IP/NAMES aren't set (set defaults here)
-
+echo $LOGFILE
 if [ -z ${CATKIN} ];
 then
-	echo "Usage: base_launch.sh <-c|--catkin path to catkin workspace> [-l|--logfile logfile]"
-	echo "OR  base_launch.sh <-c|--catkin path to catkin workspace> [-l|--logfile logfile] [-b basehostname] [-bip baseip] [-r robohostname] [-rip roboip]"
+    echo "ERROR: Must provide path to catkin workspace"
+	echo "Usage: base_launch.sh <-c|--catkin path to catkin workspace> [-l|--logfile logfile] [-b basehostname] [-bip baseip] [-r robohostname] [-rip roboip]"
     exit 1
+    # TODO: Make sure $CATKIN is a valid directory
 fi
-
+# Set default log file if not provided with one
 if [ -z ${LOGFILE} ];
 then
     LOGFILE="tmp-logfile.txt"
 fi
-# TODO:
-# 1) Always overwrite files: /etc/network/interfaces /etc/hosts and /etc/hostname
-#    with the default ones in our repo (still need to make)
-# 2) IF network command line args were set, use sed to edit the overwritten files
+#######################
+# Network configuration
+#######################
+# Make backup copies of current /etc/hosts /etc/hostname /etc/network/interfaces for backup with which to restore later
+OLDEST="" # Mark oldest copies in this directory so they don't accidentally get overwritten if script is run dirty
+if [ -d utils/netconfig/backups/ ]; then
+   # If the backups directory is empty, these are the oldest backups
+   [ "$(ls -A utils/netconfig/backups/)" ] || OLDEST="-oldest"
+fi
+cp /etc/hosts ./utils/netconfig/backups/hosts$OLDEST
+cp /etc/hostname ./utils/netconfig/backups/hostname$OLDEST
+cp /etc/network/interfaces ./utils/netconfig/backups/interfaces$OLDEST
 
+# Overwrite files: /etc/network/interfaces /etc/hosts and /etc/hostname
+# with the default ones defined in ./utils/netconfig/
+cp ./utils/netconfig/hosts /etc/hosts
+cp ./utils/netconfig/basehostname /etc/hostname
+cp ./utils/netconfig/interfaces /etc/network/interfaces
+
+# TODO:
+# If network command line args were set, then use sed to edit the overwritten files
+
+# TODO: 
+# Check for dirty exit!!, replace /etc/hosts, hostname and interfaces files with backups in the kill script 
 #diff files
 #if exit code is 0 or 2 bad condition
 EXITCODE=$(diff utils/netconfig/hosts /etc/hosts)$?
